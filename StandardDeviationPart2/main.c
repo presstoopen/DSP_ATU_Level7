7be7a6d0f9dd6f3b70eb79973632e60ddb30ff4f
presstoopen <145674614+presstoopen@users.noreply.github.com>
2024-10-09 17:05:34 +0100

    standard deviation part 2 initial

---
+++ StandardDeviationPart2/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+float data[512];
+float N = 512;
+float sum = 0;
+float mean = 0;
+float sum2 = 0;
+float SD = 0;
+int i;
+
+int main()
+{
+    for(i=0; i<N; i++)
+    {
+        data[i] = rand() % 100;
+    }
+    
+    for(i=0; i<N; i++)
+    {
+        sum += data[i];
+    }
+
+    mean = sum / N;
+
+    for(i=0; i<N; i++)
+    {
+        sum2 += pow(data[i] - mean,2);
+    }
+
+    SD = sqrt(sum2 / (N - 1));
+
+    printf("Standard Deviation = %0.2f", SD);
+
+    return 0;
+}
